31ede07baee1a57e661cdf5bd4b7e261a85f894a
Indranil Halder <indranilhalder.dev@gmail.com>
2025-04-17 17:07:56 +0530

    Update seed data

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,16 @@ const seedDB = async () => {
     const camp = new Campground({
       title: `${pickRandomElement(descriptors)} ${pickRandomElement(places)}`,
       location: `${cities[randomThousand].city}, ${cities[randomThousand].state}`,
-      image: `https://picsum.photos/400?random=${Math.random()}`,
+      images: [
+        {
+          url: `https://picsum.photos/400?random=${Math.random()}`,
+          fileName: crypto.randomUUID(),
+        },
+        {
+          url: `https://picsum.photos/400?random=${Math.random()}`,
+          fileName: crypto.randomUUID(),
+        },
+      ],
       author: "67ffc3aa0f941232fb5f4b5f",
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat efficitur tellus, non ullamcorper orci. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce dapibus varius velit. Donec tempor ipsum vel finibus cursus. Nullam euismod, magna ut faucibus fermentum, quam neque ornare ante, placerat tempor tortor sapien in nibh. Maecenas ultrices in elit sed gravida. Aliquam eget dui libero. Donec ac odio et enim tincidunt tincidunt. Praesent aliquam ullamcorper arcu in porta. Pellentesque ut neque velit.",
